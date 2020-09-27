4618daaf0f7942a275b99fb037731fcc9320ea9a
John <j44.young@connect.qut.edu.au>
2020-09-27 16:09:38 +1000

    first commit

---
+++ helloworld.cbl
@@ -0,0 +1,7 @@
+      * This is my first program in COBOL !
+                
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. hello.
+        PROCEDURE DIVISION.
+           DISPLAY "Hello World!".
+           STOP RUN.
