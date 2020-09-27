de33b162a3bca2c0cf16c2cdf8b3c8a5b96292b3
John <j44.young@connect.qut.edu.au>
2020-09-27 16:21:48 +1000

    Hello World 10 times

---
+++ helloworldtimes10.cbl
@@ -0,0 +1,9 @@
+      * Now to display "hello world" many times
+
+          IDENTIFICATION DIVISION.
+          PROGRAM-ID.  HELLOTIMESTEN.
+          PROCEDURE DIVISION.
+           PERFORM 10 TIMES
+           DISPLAY "Hello World"
+           END-PERFORM
+           STOP RUN.
